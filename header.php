7e636bd2c68604db1c7b763612551dde33d8e58a
Basar Ballioz <balliozbasar@gmail.com>
2020-12-12 16:40:58 +0300

    session ayarlarý tekrar duzenlendi

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
+<?php session_start();?>
 <!doctype html>
 <html>
-
 <head>
 <title>K-Has Discusisson Platform</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,6 +12,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="js/script.js"></script>
+
 </head>
 
 <body>
@@ -24,16 +25,15 @@
       <a href="create_cat.php">Add A New Category</a>
       <a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars"></i></a>
       <div class="topnav-right">
-  <?php 
-      session_start();
+      <?php 
       if(isset($_SESSION['signed_in']))  {
         echo '<a class="topnav-right" href="signout.php">Logout</a></div>';
       }
       else {
         echo '<a class="topnav-right" href="signin.php">Login</a>';
         echo '<a class="topnav-right" href="signup.php">Register</a></div>';
-      }
-      ?>
+      }?>
+  
     </div>
     </div>
   <div id="userbar">
